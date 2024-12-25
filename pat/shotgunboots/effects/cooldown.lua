310421f4c65df1fd29878e6def31e5496cb88e2e
patman <27874300+patmanf@users.noreply.github.com>
2024-12-24 19:20:26 -0600

    randomly pitched fire & reload sounds, muzzleflash light fades nicely

--- pat/shotgunboots/effects/cooldown.lua
+++ pat/shotgunboots/effects/cooldown.lua
@@ -6,9 +6,13 @@ function update(dt)
 	animator.setFlipped(mcontroller.facingDirection() == -1)
 
 	local state = animator.animationState("fire")
+
+	if state ~= self.currentState then
+		changedState(state)
+	end
 	
 	if state == "off" then
-		return script.setUpdateDelta(0)
+		return
 	end
 
 	if state == "wait" and mcontroller.onGround() then
@@ -16,4 +20,13 @@ function update(dt)
 	end
 
 	status.addEphemeralEffect("pat_shotgunboots")
+end
+
+function changedState(state)
+	self.currentState = state
+	
+	if animator.hasSound(state) then
+		animator.setSoundPitch(state, sb.nrand(0.06, 1))
+		animator.playSound(state)
+	end
 end
